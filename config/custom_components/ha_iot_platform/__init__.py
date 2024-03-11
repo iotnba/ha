5961360ef3fbe8302093f79ec35a933879f0d5e7
327570221 <327570221@qq.com>
2024-03-11 08:06:39 +0000

    应答代码

--- config/custom_components/ha_iot_platform/__init__.py
+++ config/custom_components/ha_iot_platform/__init__.py
@@ -14,13 +14,13 @@ from .publisher import connect_mqtt
 
 
 # The domain of your component. Should be equal to the name of your component.
-DOMAIN = 'ha_iot_platform'
+DOMAIN = "ha_iot_platform"
 
 # HA的restful服务地址，除非自定义了端口，一般不用改
-HA_REST_URL = 'http://127.0.0.1:8123'
+HA_REST_URL = "http://127.0.0.1:8123"
 
 client_id = "yyw_test_publisher"  # 设定唯一设备号，不设则mqtt随机生成
-topic = "yyw_test_mqtt"  # 自定义一个Topic
+topic = "$sys/OSTx26punO/chengyan001/thing/property/post"  # 自定义一个Topic
 
 _LOGGER = logging.getLogger()
 
@@ -32,7 +32,7 @@ class HaIotPlatform:
 
     async def handle_ha_event(self, event: Event) -> None:
         _LOGGER.warning("test!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        msg = f"messages: {event}"
+        msg = {"id": "1000", "version": "1.0", "params": {"DeviceNum": {"value": 1}}}
         result = self.publisher.publish(topic, msg)  # 指定信息的tpoic和信息内容，并发送
         # result: [0, 1]
         status = result[0]  # 解析响应内容
